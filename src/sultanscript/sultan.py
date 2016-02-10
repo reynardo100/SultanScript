aa36f841dd2fee2daadc90f2e398d547f539684b
reynardo100 <reynardo@live.com>
2016-02-10 16:01:27 +0700

    Update sultan.py

--- src/sultanscript/sultan.py
+++ src/sultanscript/sultan.py
@@ -1,1 +1,23 @@
+import os
+import sys
 
+
+def sultan():
+  if len(sys.argv) != 2:
+        print("Invalid usage. Provide a SultanScript file name to compile and run")
+        print("Example: Sultan trump_file.tr")
+        return
+
+    if not os.path.isfile(sys.argv[1]):
+        print("Invalid file specified")
+        return
+
+    
+    
+   
+
+    # Compile and go
+    Compiler().compile(sys.argv[1])
+
+if __name__ == "__sultan__":
+    sultan()
